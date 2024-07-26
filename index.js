0420b3dfaf77194f95c521da2453815b36bddb00
Yashika118 <kumariyashika78@gmail.com>
2024-07-26 22:17:54 +0530

    css of home page  is complete

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ const port=3000;
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
 })
+
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+})
