4190c2e265abd04fc9cd350955268edf3145f443
Yashika118 <kumariyashika78@gmail.com>
2024-07-31 22:06:18 +0530

    data is fetch from search button

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ app.listen(port, ()=>{
 
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/",(req,res)=>{
+    
     res.render("index.ejs");
+   
+})
+
+app.get("/search",(req,res)=>{
+    res.render("search.ejs");
+    
+})
+
+app.post("/search",async(req,res)=>{
+    // const { body: { yk_search: abc} } = req;
+    // const abc={name:req.body["yk_search"]};
+    const abc=req.body["yk_search"];
+    console.log(abc);
 })
