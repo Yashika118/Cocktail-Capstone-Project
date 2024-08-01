dac660d29b2d291d8b016432bcc572b853c913df
Yashika118 <kumariyashika78@gmail.com>
2024-08-01 20:43:08 +0530

    in recipe.ejs flip card is not working

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import axios from "axios";
 const app=express();
 const port=3000;
 
+const API_URL="https://www.thecocktaildb.com/api/json/v1/1";
+
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
 })
@@ -24,9 +26,46 @@ app.get("/search",(req,res)=>{
     
 })
 
+
 app.post("/search",async(req,res)=>{
     // const { body: { yk_search: abc} } = req;
     // const abc={name:req.body["yk_search"]};
-    const abc=req.body["yk_search"];
-    console.log(abc);
+    const cocktail_name=req.body["yk_search"];
+    // console.log(cocktail_name);
+
+
+    // AXIOS
+    // console.log(`${API_URL}/search.php?s=${cocktail_name}`);
+    try{
+        const response=await axios.get(`${API_URL}/search.php?s=${cocktail_name}`);
+        const result=response.data;
+        // console.log(result);
+        res.render("search.ejs",{result:result});
+        
+      }catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+          error: "No activities that match your criteria",
+        });
+      }
+
+})
+
+app.post("/recipe",async(req,res)=>{
+    const id=req.body["drink_id"];
+    // console.log(id);
+    
+    try{
+        const response=await axios.get(`${API_URL}/lookup.php?i=${id}`);
+        const result=response.data;
+        // console.log(result);
+        res.render("recipe.ejs",{result:result});
+        
+      }catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+          error: "No activities that match your criteria",
+        });
+      }
+
 })
